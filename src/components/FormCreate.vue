b256cb90070da574681aeb560cb406b7005345fc
Stanislav Shastov <shastov.stas1991@gmail.com>
2025-01-01 09:14:47 +0300

    :value description and title

--- src/components/FormCreate.vue
+++ src/components/FormCreate.vue
@@ -4,11 +4,12 @@
     <form action="" class="form">
         <label for="name">
             <span>Название:</span>
-            <input type="text" name="name" id="name">
+            <input type="text" name="name" id="name" :value="newCard.title" @input="setTitle">
         </label>
         <label for="description">
             <span>Описание:</span>
-            <input type="text" name="description" id="description">
+            <input type="text" name="description" id="description" :value="newCard.description" 
+            @input="setDescription">
         </label>
         <button class="btn" type="submit">Добавить</button>
     </form>
@@ -16,7 +17,24 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const newCard = ref({
+  id: 0,
+  title: '',
+  description: '',
+  completed: false,
+})
+
+const setTitle = (event) => {
+  const value = event.target.value;
+  newCard.value.title = value;
+}
+
+const setDescription = (event) => {
+  const value = event.target.value;
+  newCard.value.description = value;
+}
 </script>
 
 <style scope>
